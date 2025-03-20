041e835defd14d765299058b21f7b43a2f4eb21d
Nithishaavula <nithishaavula12101999@gmail.com>
2025-03-20 16:04:31 +0530

    python practice

---
+++ practice.py
@@ -0,0 +1,55 @@
+# num=input('enter a number')
+# write a program to check given  number is positive or negative or zero
+num=-1
+if num == 0:
+    print('zero')
+elif(num>0):
+    print('positive')
+else:
+    print('negative')
+# Determine if a number is even or odd.
+num1=3
+if(num1%2==0):
+    print('given number is even')
+else:
+    print('given number is odd')
+# check if a person is eligible to vote
+age=17
+if(age>=18):
+    print("he/she eligible to vote")
+else:
+    print('he/she is not eligible vote')
+# write a program to find the greatest of two numbers
+num2=19
+num3=19
+if(num2>num3):
+    print('first number is greater')
+elif(num2==num3):
+    print('first and second number is same')
+else:
+    print('second number is greaters')
+#print "pass" is a student scores more than 40 marks;otherwise fail
+marks=4
+if(marks>40):
+    print("pass")
+else:
+    print("fail")
+# write  PROGRAM to display the day of a week based on a number input(1 for monday,2 for tuesday)
+# a=input('enter a number')
+a=2
+if(a==1):
+    print('monday')
+elif(a==2):
+    print('tuesday')
+elif(a==3):
+    print('wednesday')
+elif(a==4):
+    print('thursday')
+elif(a==5):
+    print('friday')
+elif(a==6):
+    print('saturday')
+elif(a==7):
+    print('sunday')
+else:
+    print("enter only 1-7 days")
